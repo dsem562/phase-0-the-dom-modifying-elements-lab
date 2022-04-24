a52405bac74161ec98594cd894058efdc329be9b
David <dsem562@gmail.com>
2022-04-24 14:06:09 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+main.remove();
+
+let element = document.createElement("div");
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "YOUR-NAME is the champion";
+
+document.body.append(element);
+element.append(newHeader);
